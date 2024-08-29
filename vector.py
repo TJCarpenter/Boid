fa68b326cbda23fa09ea72be80045a53a6cbee64
Tyler Carpenter <tylercarpenter1996@gmail.com>
2024-08-29 08:13:49 -0400

    Remove vector functions not used

--- vector.py
+++ vector.py
@@ -5,9 +5,6 @@ class Vector:
         self.x = x
         self.y = y
 
-    def __repr__(self):
-        return f"Vector({self.x}, {self.y})"
-
     def __add__(self, other):
         return Vector(self.x + other.x, self.y + other.y)
 
@@ -23,9 +20,6 @@ class Vector:
         else:
             raise ValueError("Cannot divide by zero")
 
-    def dot(self, other):
-        return self.x * other.x + self.y * other.y
-
     def magnitude(self):
         return math.sqrt(self.x**2 + self.y**2)
 
@@ -36,18 +30,8 @@ class Vector:
         else:
             return Vector(0, 0)
 
-    def perpendicular(self):
-        return Vector(-self.y, self.x)
-
     def distance_to(self, other):
         return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
-
-    def angle_with(self, other):
-        dot_product = self.dot(other)
-        magnitudes = self.magnitude() * other.magnitude()
-        if magnitudes == 0:
-            raise ValueError("Cannot calculate angle with zero-length vector")
-        return math.acos(dot_product / magnitudes)
 
     def limit(self, max_magnitude):
         magnitude = self.magnitude()
